b1a57c557f8735f493436be3dd336e637ea5df68
Tess Ferrandez <tferrand@microsoft.com>
2019-12-09 13:00:56 +0100

    day 1

---
+++ 2019/day1.py
@@ -0,0 +1,25 @@
+def calculate_fuel(mass):
+    return int(mass) // 3 - 2
+
+
+def calculate_fuel_v2(mass):
+    fuel = calculate_fuel(mass)
+    if fuel <= 0:
+        return 0
+    fuel += calculate_fuel_v2(fuel)
+    return fuel
+
+
+def puzzle1():
+    total_fuel = sum(calculate_fuel(mass) for mass in open('input/day1.txt'))
+    print("total fuel (v1): ", total_fuel)
+
+
+def puzzle2():
+    total_fuel = sum(calculate_fuel_v2(mass) for mass in open('input/day1.txt'))
+    print("total fuel (v2): ", total_fuel)
+
+
+if __name__ == "__main__":
+    puzzle1()
+    puzzle2()
