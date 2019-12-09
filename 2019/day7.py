4493637d7a12486adce08d50b700c1be959a28b1
Tess Ferrandez <tferrand@microsoft.com>
2019-12-09 22:20:54 +0100

    day 7

---
+++ 2019/day7.py
@@ -0,0 +1,66 @@
+from IntCode import IntCode, OutputInterrupt, InputInterrupt
+from itertools import permutations
+from collections import deque
+
+
+def run_sequence(sequence):
+    amp = 0
+    for phase in sequence:
+        computer = IntCode(open('input/day7.txt').readline())
+        computer.input_queue = deque([phase, amp])
+        while not computer.done:
+            try:
+                computer.run()
+            except OutputInterrupt:
+                pass
+
+        amp = computer.output_queue.popleft()
+
+    return amp
+
+
+def run_sequence_v2(sequence, program):
+    computers = list()
+
+    for phase in sequence:
+        computers.append(IntCode(program))
+        computers[-1].input_queue.append(phase)
+
+    i = -1
+    computers[0].input_queue.append(0)
+    while True:
+        i = (i + 1) % 5
+        while not computers[i].done:
+            try:
+                computers[i].run()
+            except OutputInterrupt:
+                computers[(i+1) % 5].input_queue.append(computers[i].output_queue[-1])
+                continue
+            except InputInterrupt:
+                break
+
+        if all(map(lambda x: x.done, computers)):
+            break
+
+    return computers[0].input_queue[-1]
+
+
+def puzzle1():
+    sequences = permutations(range(5))
+    max_sequence = max(sequences, key=lambda x: run_sequence(x))
+    print(max_sequence)
+    print("generates:", run_sequence(max_sequence))
+
+
+def puzzle2():
+    sequences = permutations(range(5, 10))
+    program = open('input/day7.txt').readline()
+    max_sequence = max(sequences, key=lambda x: run_sequence_v2(x, program))
+    print(max_sequence)
+    print("generates:", run_sequence_v2(max_sequence, program))
+
+
+if __name__ == "__main__":
+    puzzle1()
+    puzzle2()
+
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Code for www.adventofcode.com in Python 2019.
 
 **[Day 1](2019/day1.py)** - The Tyranny of the Rocket Equation -- Calculate Fuel
 
-**[Day 2](2019/day2.py)** - 1202 Programming Alarm -- IntCode computer - Part 1
+**[Day 2](2019/day2.py)** - 1202 Programming Alarm -- IntCode computer - Part I
 
 **[Day 3](2019/day3.py)** - Crossing Wires -- (R32,L43...)
 
@@ -15,3 +15,6 @@ Code for www.adventofcode.com in Python 2019.
 **[Day 5](2019/day5.py)** - Sunny with a Chance of Asteroids -- IntCode - Part II
 
 **[Day 6](2019/day6.py)** - Universal Orbit Map -- Calculate number of orbits A)B
+
+**[Day 7](2019/day7.py)** - Amplification Circuit -- IntCode - Part III
+
