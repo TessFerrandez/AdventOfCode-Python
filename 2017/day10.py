bc1a2812a8a84e959f7fe1d2b58a5f0b1d261325
Tess Ferrandez <tferrand@microsoft.com>
2019-12-18 22:12:06 +0100

    2017 day 10

---
+++ 2017/day10.py
@@ -0,0 +1,53 @@
+from functools import reduce
+
+
+def calc_knot_hash(nums: list, lengths: list, pos: int, skip: int) -> (int, int):
+    for l in lengths:
+        to_reverse = []
+        for i in range(l):
+            n = (pos + i) % 256
+            to_reverse.append(nums[n])
+        to_reverse.reverse()
+        for i in range(l):
+            n = (pos + i) % 256
+            nums[n] = to_reverse[i]
+        pos = (pos + l + skip) % 256
+        skip += 1
+
+    return pos, skip
+
+
+def puzzle1():
+    input_string = open('input/day10.txt').read().strip()
+    lengths = [int(n) for n in input_string.split(',')]
+
+    nums = [n for n in range(256)]
+    calc_knot_hash(nums, lengths, 0, 0)
+
+    print("puzzle 1:", nums[0] * nums[1])
+
+
+def puzzle2():
+    input_string = open('input/day10.txt').read().strip()
+    lengths = [ord(char) for char in input_string]
+    lengths.extend([17, 31, 73, 47, 23])
+    nums = [n for n in range(256)]
+    pos, skip = 0, 0
+
+    for _ in range(64):
+        pos, skip = calc_knot_hash(nums, lengths, pos, skip)
+
+    dense = []
+    for i in range(16):
+        sub_slice = nums[16 * i: 16 * i + 16]
+        dense.append('%02x'%reduce((lambda x, y: x ^ y), sub_slice))
+    print("puzzle 2:", ''.join(dense))
+
+
+def puzzles():
+    puzzle1()
+    puzzle2()
+
+
+if __name__ == "__main__":
+    puzzles()
--- README.md
+++ README.md
@@ -66,6 +66,8 @@ Code for www.adventofcode.com in Python 2019.
 
 **[Day 9](2017/day9.py)** - Stream Processing -- <garbage> {group}
 
+**[Day 10](2017/day10.py)** - Knot Hash -- Elves hashing function
+
 ## 2016
 
 **[Day 1](2016/day1.py)** - No Time for a Taxicab -- Rotate Right and Left
