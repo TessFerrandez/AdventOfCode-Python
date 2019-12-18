243997af452a8888064702899be4aeafb3e4baa7
Tess Ferrandez <tferrand@microsoft.com>
2019-12-18 23:34:55 +0100

    2017 day 11

---
+++ 2017/day11.py
@@ -0,0 +1,32 @@
+moves = {
+    'n': (0, 1),
+    'ne': (0.5, 0.5),
+    'nw': (-0.5, 0.5),
+    's': (0, -1),
+    'se': (0.5, -0.5),
+    'sw': (-0.5, -0.5)
+}
+
+
+def do_moves(input_steps: list) -> (int, int):
+    max_dist = 0
+    x, y = 0.0, 0.0
+    for step in input_steps:
+        x += moves[step][0]
+        y += moves[step][1]
+        dist = abs(x) + abs(y)
+        max_dist = max(max_dist, dist)
+
+    end_dist = abs(x) + abs(y)
+    return int(end_dist), int(max_dist)
+
+
+def puzzles():
+    input_steps = open("input/day11.txt").read().strip().split(',')
+    end_dist, max_dist = do_moves(input_steps)
+    print("end distance:", end_dist)
+    print("max distance:", max_dist)
+
+
+if __name__ == "__main__":
+    puzzles()
--- README.md
+++ README.md
@@ -68,6 +68,8 @@ Code for www.adventofcode.com in Python 2019.
 
 **[Day 10](2017/day10.py)** - Knot Hash -- Elves hashing function
 
+**[Day 11](2017/day11.py)** - Hex Ed -- Reduce hexagonal path
+
 ## 2016
 
 **[Day 1](2016/day1.py)** - No Time for a Taxicab -- Rotate Right and Left
