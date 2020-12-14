86a2fbe160a273b93e3f467db591712fef1a7110
Tess Ferrandez <tess.ferrandez@microsoft.com>
2020-12-14 14:24:17 +0100

    refactor 2015

--- 2015/day24.py
+++ 2015/day24.py
@@ -1,33 +1,63 @@
-import itertools
+import pytest
+from typing import List
+from itertools import combinations
+from functools import reduce
 
 
-def get_product(lst: list) -> int:
-    product = 1
-    for item in lst:
-        product *= item
-    return product
+@pytest.mark.parametrize('packages, expected',
+                         [
+                             ([1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 99),
+                         ])
+def test_part1(packages: List[int], expected: int):
+    assert part1(packages) == expected
 
 
-def puzzles():
-    numbers = [int(num.strip()) for num in open("input/day24.txt").readlines()]
-    # total = 1548
-    # Group size = 516 (1548 / 3)
-    # sample group 113, 109, 107, 103, 71, 13 (6)
-    sequence_products = [
-        get_product(seq)
-        for seq in itertools.combinations(numbers, 6)
-        if sum(seq) == 516
-    ]
-    print("lowest quantum entanglement:", min(sequence_products))
-    # Group size = 387 (1548 / 4)
-    # sample group 113, 109, 107, 53, 5 (5)
-    sequence_products = [
-        get_product(seq)
-        for seq in itertools.combinations(numbers, 5)
-        if sum(seq) == 387
-    ]
-    print("lowest quantum entanglement:", min(sequence_products))
+@pytest.mark.parametrize('packages, expected',
+                         [
+                             ([1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 44),
+                         ])
+def test_part2(packages: List[int], expected: int):
+    assert part2(packages) == expected
+
+
+def parse_input(filename: str) -> List[int]:
+    return [int(package.strip()) for package in open(filename).readlines()]
+
+
+def find_smallest_qe(packages: List[int], n_groups: int) -> int:
+    total_weight = sum(packages)
+    group_weight = total_weight // n_groups
+
+    smallest_combos = []
+    for length in range(0, len(packages) + 1):
+        for combo in combinations(packages, length):
+            if sum(combo) == group_weight:
+                smallest_combos.append(combo)
+        if smallest_combos:
+            break
+
+    smallest_qe = float('inf')
+    for combo in smallest_combos:
+        qe = reduce((lambda x, y: x * y), combo)
+        if qe < smallest_qe:
+            smallest_qe = qe
+
+    return smallest_qe
+
+
+def part1(packages: List[int]) -> int:
+    return find_smallest_qe(packages, 3)
+
+
+def part2(packages: List[int]) -> int:
+    return find_smallest_qe(packages, 4)
+
+
+def main():
+    packages = parse_input('input/day24.txt')
+    print(f'Part 1: {part1(packages)}')
+    print(f'Part 2: {part2(packages)}')
 
 
 if __name__ == "__main__":
-    puzzles()
+    main()
