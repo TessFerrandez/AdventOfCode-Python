7cf79fd777f0ba76bfb11b66af27b6c855400b9e
Tess Ferrandez <tess.ferrandez@microsoft.com>
2020-12-14 15:36:09 +0100

    refactor 2015

--- 2015/day25.py
+++ 2015/day25.py
@@ -4,25 +4,24 @@ def get_order(col: int, row: int) -> int:
         order += i
     for i in range(col, col + row - 1):
         order += i
-
     return order
 
 
-def calculate_code(order: int) -> int:
+def part1(col: int, row: int) -> int:
     start = 20151125
     times = 252533
     divisor = 33554393
 
+    order = get_order(col, row)
     code = start
     for i in range(order - 1):
         code = (code * times) % divisor
     return code
 
 
-def puzzles():
-    order = get_order(3019, 3010)
-    print("code:", calculate_code(order))
+def main():
+    print(f'Part 1: {part1(3019, 3010)}')
 
 
 if __name__ == "__main__":
-    puzzles()
+    main()
