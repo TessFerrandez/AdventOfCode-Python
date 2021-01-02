2b3045925e23cc3429227b9929f30dbb93e697ae
Tess Ferrandez <tess.ferrandez@microsoft.com>
2021-01-02 13:18:18 +0100

    2018 day 18

--- 2018/day18.py
+++ 2018/day18.py
@@ -1,3 +1,5 @@
+from typing import List
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -5,73 +7,75 @@ OPEN = 0
 TREE = 1
 LUMBER = 2
 
-SIZE = 10
+
+def parse_input(filename: str) -> List[str]:
+    lines = open(filename).read().splitlines()
+    return lines
 
 
-def print_grid(grid: np.array):
-    symbols = {OPEN: ".", TREE: "|", LUMBER: "#"}
-    for line in grid:
-        for tile in line:
-            print(symbols[tile], end="")
-        print()
-    print()
+def iterate(grid: np.array):
+    original = grid.copy()
+    height, width = len(grid), len(grid[0])
+
+    for y in range(height - 2):
+        for x in range(width - 2):
+            sub = original[y: y + 3, x: x + 3]
+            if original[y + 1][x + 1] == OPEN:
+                if np.count_nonzero(sub[sub == TREE]) >= 3:
+                    grid[y + 1][x + 1] = TREE
+            elif original[y + 1][x + 1] == TREE:
+                if np.count_nonzero(sub[sub == LUMBER]) >= 3:
+                    grid[y + 1][x + 1] = LUMBER
+            elif original[y + 1][x + 1] == LUMBER:
+                if np.count_nonzero(sub[sub == LUMBER]) >= 2 and np.count_nonzero(sub[sub == TREE]) >= 1:
+                    grid[y + 1][x + 1] = LUMBER
+                else:
+                    grid[y + 1][x + 1] = OPEN
 
 
-def parse_input(filename: str) -> np.array:
-    global SIZE
+def part1(lines: List[str]) -> int:
+    grid = generate_grid(lines)
 
-    lines = [line.strip() for line in open(filename).readlines()]
-    SIZE = len(lines)
-    grid = np.zeros((SIZE, SIZE))
+    for i in range(10):
+        iterate(grid)
 
-    symbols = {".": OPEN, "|": TREE, "#": LUMBER}
+    return np.count_nonzero(grid[grid == 1]) * np.count_nonzero(grid[grid == 2])
+
+
+def part2(lines: List[str]) -> int:
+    grid = generate_grid(lines)
+    resource_values = {}
+
+    fig, axs = plt.subplots(4, 14)
+
+    # pattern repeats from 417 to 444
+    for i in range(0, 473):
+        iterate(grid)
+        if 417 <= i:
+            axs[(i - 417) // 14, (i - 417) % 14].imshow(grid)
+            resource_value = np.count_nonzero(grid[grid == 1]) * np.count_nonzero(grid[grid == 2])
+            resource_values[i - 417] = resource_value
+    plt.show()
+
+    return resource_values[((1000000000 - 417 - 1) % 56)]
+
+
+def generate_grid(lines: List[str]) -> np.array:
+    height, width = len(lines), len(lines[0])
+    grid = np.zeros((height + 2, width + 2))
     for y, line in enumerate(lines):
-        for x, tile in enumerate(line):
-            grid[y][x] = symbols[tile]
-
+        for x, ch in enumerate(line):
+            if ch == '#':
+                grid[y + 1][x + 1] = LUMBER
+            elif ch == '|':
+                grid[y + 1][x + 1] = TREE
     return grid
 
 
-def get_adjacent(grid: np.array, x: int, y: int, tile: int):
-    x_from, x_to = max(0, x - 1), min(x + 1, SIZE)
-    y_from, y_to = max(0, y - 1), min(y + 1, SIZE)
-    adjacent = grid[y_from : y_to + 1, x_from : x_to + 1]
-    return np.sum(adjacent == tile)
-
-
-def puzzle1(grid: np.array, minutes: int) -> int:
-    for i in range(minutes):
-        grid_copy = grid.copy()
-        for y, row in enumerate(grid):
-            for x, tile in enumerate(row):
-                if tile == OPEN:
-                    if get_adjacent(grid_copy, x, y, TREE) >= 3:
-                        grid[y][x] = TREE
-                elif tile == TREE:
-                    if get_adjacent(grid_copy, x, y, LUMBER) >= 3:
-                        grid[y][x] = LUMBER
-                elif tile == LUMBER:
-                    if (
-                        get_adjacent(grid_copy, x, y, LUMBER) >= 2
-                        and get_adjacent(grid_copy, x, y, TREE) >= 1
-                    ):
-                        grid[y][x] = LUMBER
-                    else:
-                        grid[y][x] = OPEN
-        # print_grid(grid)
-        if i % 1000 == 0:
-            print(i)
-
-    trees = np.sum(grid == TREE)
-    lumber = np.sum(grid == LUMBER)
-    return trees * lumber
-
-
 def main():
-    grid = parse_input("input/day18.txt")
-    puzzle1_result = puzzle1(grid, 10)
-    print(f"Part 1: {puzzle1_result}")
-    print("Part 2: Takes an unreasonable long time - have to get back to this")
+    lines = parse_input('input/day18.txt')
+    print(f'Part 1: {part1(lines)}')
+    print(f'Part 2: {part2(lines)}')
 
 
 if __name__ == "__main__":
